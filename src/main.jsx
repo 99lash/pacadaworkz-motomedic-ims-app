d607494cb0650141f61bed17e928f2380f7798cd
GABlane <ofiangga.bscs@gmail.com>
2025-12-26 11:17:43 +0800

    Google Auth Implemented, just fixing some css bugs

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,16 @@ import { BrowserRouter } from 'react-router-dom'; // Para sa Routing
 import { Provider } from 'react-redux'; // Para sa Redux
 import { store } from './store/store';  // Galing sa Hakbang 3
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}> {/* I-wrap sa Redux Provider */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </GoogleOAuthProvider>
     </Provider>
   </React.StrictMode>,
 );
